b0d42deac733f9cb9a6d60b5f43d5e714aa452d6
1067441348 <1067441348@qq.com>
2017-12-07 15:42:23 +0800

    添加picasso

--- app/src/main/java/com/example/syl/myapplication_test/activity/GildeActivity.java
+++ app/src/main/java/com/example/syl/myapplication_test/activity/GildeActivity.java
@@ -6,9 +6,10 @@ import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 import com.example.syl.myapplication_test.R;
+import com.squareup.picasso.Picasso;
 
 public class GildeActivity extends AppCompatActivity {
-    private ImageView img_gilde;
+    private ImageView img_gilde,img_picasso;
     private String url = "http://wx2.sinaimg.cn/mw690/ac38503ely1fesz8m0ov6j20qo140dix.jpg";
 
     @Override
@@ -16,16 +17,31 @@ public class GildeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gilde);
         initView();
-        getPic();
+        getGlide();
+        getPicasso();
     }
 
-    private void getPic() {
-        Glide.with(GildeActivity.this).load(url).into(img_gilde);
+    private void getPicasso() {
+        Picasso.with(GildeActivity.this).
+                load(url)
+                .placeholder(R.mipmap.ic_launcher)
+                .resize(500,500)
+                .into(img_picasso);
+    }
+
+    private void getGlide() {
+        Glide.with(GildeActivity.this)
+                .load(url)
+                .override(500,500)//尺寸
+                .placeholder(R.mipmap.ic_launcher)//加载中
+                .error(R.mipmap.ic_launcher)//错误
+                .into(img_gilde);
 
     }
 
     private void initView() {
         img_gilde = (ImageView) findViewById(R.id.img_gilde);
+        img_picasso = (ImageView) findViewById(R.id.img_picasso);
         
     }
     
